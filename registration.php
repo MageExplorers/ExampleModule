64e8f802f1d299cbcc469ee5a9eb6ded819825ef
Bradley R Clampitt <bradclampitt@me.com>
2022-08-08 08:58:56 -0400

    1st Commit from ipad

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ExampleModule
+ *
+ * @author:      Bradley R. Clampitt
+ * @Email:       bradley@magentoexplorers.com 
+ * @category:    MagentoExplorers
+ * @package:     MageExplorers_ExampleModule
+ * @Date:        2022-08-05
+ *
+*/
+\Magento\Framework\Component\ComponentRegistrar::register(
+   \Magento\Framework\Component\ComponentRegistrar::MODULE,
+   'MageExplorers_ExampleModule',
+   __DIR__
+);
